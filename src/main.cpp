95534ef5d5b7f862d57ffe2abd0046c75a950f23
birchtree02 <edbirchy664+gh@gmail.com>
2021-08-21 11:04:52 +0100

    implement working debounce function

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@
 #define NUM_BUTTONS 3
 #define KB_NAME "PageTurner"
 #define LONG_PRESS_TIME 500 // ms
+#define DEBOUNCE_TIME 100
 
 int button_pins[3] = {18, 25, 34};
 int button_states[3] = {0, 0, 0};
+int last_pressed[3] = {0, 0, 0};
 std::chrono::high_resolution_clock::time_point t0;
 std::chrono::high_resolution_clock::time_point t1;
 
@@ -27,24 +29,26 @@ void setup() {
 }
 
 void loop() {
-  if(bleKeyboard.isConnected()) {
+  // if(bleKeyboard.isConnected()) {
     // Serial.println();
     for (int i=0; i<NUM_BUTTONS; i++) {
       // 
       // Serial.print(digitalRead(button_pins[i]));
       // 
-      if (digitalRead(button_pins[i]) == LOW && button_states[i] == 0) {
-        delay(100);
-        button_states[i] = 1;
-        Serial.print(i);
-      } else {
-        if (button_states[i] == 1 && digitalRead(button_pins[i]) == HIGH) {
-          button_states[i] = 0;
+      if(millis() - last_pressed[i] > 100) {
+        last_pressed[i] = millis();
+        if (digitalRead(button_pins[i]) == LOW && button_states[i] == 0) {
+          button_states[i] = 1;
+          Serial.print(i);
+        } else {
+          if (button_states[i] == 1 && digitalRead(button_pins[i]) == HIGH) {
+            button_states[i] = 0;
+          }
         }
       }
       
     }
-  }
+  // }
 }
 
 
