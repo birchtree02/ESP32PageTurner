db73484ca7a008cebefbecb25201623839a0fb36
birchtree02 <edbirchy664+gh@gmail.com>
2021-11-04 11:32:46 +0000

    add debug stmts

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #define NUM_BUTTONS 3
 #define KB_NAME "PageTurner"
 #define LONG_PRESS_TIME 500 // ms
-#define DEBOUNCE_TIME 100
+#define DEBOUNCE_TIME 250
 
 int button_pins[3] = {18, 25, 34};
 int button_states[3] = {0, 0, 0};
@@ -26,7 +26,9 @@ void setup() {
 
 void loop() {
   if(bleKeyboard.isConnected()) {
+    Serial.println("");
     for (int i=0; i<NUM_BUTTONS; i++) {
+      Serial.print(digitalRead(button_pins[i]));
       if(millis() - last_pressed[i] > DEBOUNCE_TIME) {
         if (digitalRead(button_pins[i]) == LOW && button_states[i] == 0) {
           last_pressed[i] = millis();
@@ -37,19 +39,21 @@ void loop() {
 
           if (i==0) {
             if (millis() - last_pressed[i] > LONG_PRESS_TIME) {
-              bleKeyboard.press(KEY_SHIFT);
+              bleKeyboard.press(KEY_LEFT_SHIFT);
             }
             bleKeyboard.press(KEY_LEFT_CTRL);
             bleKeyboard.press(KEY_TAB);
           } else if (i==1) {
             for (int x=0; x<6; x++) {
               bleKeyboard.press(KEY_UP_ARROW);
-              delay(100);
+              bleKeyboard.releaseAll();
+              // delay(100);
             }
           } else if (i==2) {
             for (int x=0; x<6; x++) {
               bleKeyboard.press(KEY_DOWN_ARROW);
-              delay(100);
+              bleKeyboard.releaseAll();
+              // delay(100);
             }
           }
           bleKeyboard.releaseAll();
